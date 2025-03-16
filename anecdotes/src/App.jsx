fd855342f251585d02116d623845266512f934e9
robineisepoptahti <155745755+robineisepoptahti@users.noreply.github.com>
2025-03-16 12:29:01 +0200

    Added json server(6.14)

--- anecdotes/src/App.jsx
+++ anecdotes/src/App.jsx
@@ -2,8 +2,21 @@ import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import anecdoteServices from './services/anecdotes'
+import {setAnecdotes} from './reducers/anecdoteReducer'
+
 
 const App = () => {
+  const dispatch = useDispatch()
+
+
+  useEffect(() => {
+    anecdoteServices.getAll().then(notes => {
+      dispatch(setAnecdotes(notes))
+    })
+  }, [dispatch])
   return (
     <div>
     <h2>Anecdotes</h2>
--- anecdotes/src/reducers/anecdoteReducer.js
+++ anecdotes/src/reducers/anecdoteReducer.js
@@ -1,28 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit'
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-const initialState = anecdotesAtStart.map(asObject)
-
-
 const anecdotesSlice = createSlice({
   name: 'anecdotes',
-  initialState: initialState,
+  initialState: [],
   reducers: {
     
     anecdoteReducer(state, action){
@@ -34,9 +16,13 @@ const anecdotesSlice = createSlice({
       },
 addVote(state, action){
   return state.map(entry => entry.id !== action.payload ? entry : {...entry, votes: entry.votes + 1}).sort((a, b) => b.votes - a.votes)
+},
+setAnecdotes(state, action)
+{
+  return action.payload
 }
 }
 })
 
-export const {anecdoteReducer, addVote} = anecdotesSlice.actions
+export const {anecdoteReducer, addVote, setAnecdotes} = anecdotesSlice.actions
 export default anecdotesSlice.reducer
